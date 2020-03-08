1d54b488455c9991baebe2996dac920f050583f3
rwegter <rwegter@pdx.edu>
2020-03-07 16:23:40 -0800

    Init 2-bit predictor.

--- predictor.cc
+++ predictor.cc
@@ -2,30 +2,47 @@
  * Description: This file defines the two required functions for the branch predictor.
 */
 
+int global_predict_bit;
+
 #include "predictor.h"
 
 
+	inline bool get_global_prediction(void){
+
+    	bool prediction = false;
+
+    	//get_global_predict_bit();
+
+    	if (global_predict_bit & 0x3 >> 1){
+    		prediction = false;
+    	}
+    	else
+    		prediction = true;
+
+    	return prediction;
+	}
+	inline void update_global_predictor(bool taken){
+
+		if(taken == true && global_predict_bit < 3){
+			global_predict_bit++;
+		}
+		else if(taken == false && global_predict_bit > 0){
+			global_predict_bit--;
+		}
+
+	}
 
     bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os)
         {
-		/* replace this code with your own */
-            bool prediction = false;
+    	bool prediction = false;
 
-			printf("%0x %0x %1d %1d %1d %1d ",br->instruction_addr,
-			                                br->branch_target,br->is_indirect,br->is_conditional,
-											br->is_call,br->is_return);
-            if (br->is_conditional)
-                prediction = true;
-            return prediction;   // true for taken, false for not taken
+    	//get_global_predict_bit();
+
+    	//prediction = get_global_prediction();
+
+    	return prediction;
+
         }
-
-
-
-
-
-
-
-
 
 
 
@@ -35,6 +52,7 @@
     void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os, bool taken)
         {
 		/* replace this code with your own */
-			printf("%1d\n",taken);
+
+    	update_global_predictor(taken);
 
         }
